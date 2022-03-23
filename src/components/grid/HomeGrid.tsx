2997736446f598e83c3460a1c2b74b5aba01ce31
c090695 Eduardo Silva <eduardo-rodrigues.silva@caixa.gov.br>
2022-03-23 11:15:26 -0300

    Logo

--- src/components/grid/HomeGrid.tsx
+++ src/components/grid/HomeGrid.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Link, Paper, Stack, Typography } from '@mui/material';
 import Image from 'next/image'
+import Logo from './logo.svg';
 
 export default function HomeGrid() {
 
@@ -23,11 +24,11 @@ export default function HomeGrid() {
             Sistema de Apoio a Distribuição de Metas
           </Typography>
           <Box sx={{ml: 'auto', mr: 'auto', maxWidth:'580px', maxHeight:'400px'}}>
-            <Image src="/objetivos.jpg" alt="me" width="600" height='400' layout='responsive'/>
+            <Image src={Logo}  alt="me" width="600" height='400' layout='responsive'/>
           </Box>
 
 
-          <Typography variant="h5" align="center" color="text.secondary" paragraph>
+          <Typography variant="h5" sx={{mt: '10px'}} align="center" color="text.secondary" paragraph>
           Bem vindo!
           O prazo de ajustes é ate <b>08/05 - 14hs</b>
           </Typography>
