2356bb76750c7b9dae4da4fea59dfff19d5654f3
c090695 Eduardo Silva <eduardo-rodrigues.silva@caixa.gov.br>
2022-03-25 16:16:05 -0300

    Fix for push

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = {
   reactStrictMode: true,
     env: {
     customKey: 'my-value',
-    //baseURL:  'https://sr2625-metas-api.herokuapp.com/api/v1'
-    baseURL:  'http://localhost:7000/api/v1'
+    baseURL:  'https://sr2625-metas-api.herokuapp.com/api/v1'
+    //baseURL:  'http://localhost:7000/api/v1'
 
   },
    webpack(config) {
